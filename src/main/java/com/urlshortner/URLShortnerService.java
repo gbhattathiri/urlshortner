0e070456833d7e89b356d42328279d42e45fd10f
gbhattathiri <ganesh.bhattathiri@gmail.com>
2016-10-10 18:14:03 +0530

    First Checkin
    Authentication, Regestration Service

---
+++ src/main/java/com/urlshortner/URLShortnerService.java
@@ -0,0 +1,22 @@
+
+package com.urlshortner;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/tinyURL")
+public class URLShortnerService {
+	
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+
+		String output = "Jersey say : " + msg;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+}
